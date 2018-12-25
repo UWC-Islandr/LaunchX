37629ad6f19e0d8ac8cd70ce1d105aff04064460
SkyLu <2924312854@qq.com>
2018-12-25 10:54:20 +0800

    refactor face recognition process

--- face_recognition_test/face.py
+++ face_recognition_test/face.py
@@ -1,50 +1,86 @@
 from PIL import Image, ImageDraw
 import face_recognition
+import numpy
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file("pic/sky.jpg")
+class FaceId (object):
 
-face_locations = face_recognition.face_locations(image)
+    def __init__ (self, img_location):
 
-box = [337, 229, 411, 304]
+        self.ary_image = face_recognition.load_image_file(img_location)
+        self.pil_image = 0
+        self.box = [0] * 4
+        self.face_landmarks_list = []
 
-def padding(box, pixel):
-    box[0] -= pixel
-    box[1] -= pixel
-    box[2] += pixel
-    box[3] += pixel
+    def _locate_faces(self):
 
-padding(box, 10)
+        face_locations = face_recognition.face_locations(self.ary_image)
 
-print(str(face_locations)+'\n')
+        return face_locations[0]
 
-# Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+    def _regularize_locations(self, face_location):
 
-print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
+        tmpbox = [0] * 4
+        for i in range(4):
+            tmpbox[i] = face_location[i]
 
-# Create a PIL imagedraw object so we can draw on the picture
-pil_image = Image.fromarray(image)
+        self.box[0] = tmpbox[3]
+        self.box[1] = tmpbox[0]
+        self.box[2] = tmpbox[1]
+        self.box[3] = tmpbox[2]
 
-print(str(pil_image.size) + '\n')
+    def _padding(self, pixel):
 
-d = ImageDraw.Draw(pil_image)
+        self.box[0] -= pixel
+        self.box[1] -= pixel
+        self.box[2] += pixel
+        self.box[3] += pixel
 
-print(str(pil_image.size) + '\n')
+    def _crop_face(self):
 
-# Print the region where contains face in the picture
-region = pil_image.crop(box)
-region.show()
+        face_location = self._locate_faces()
+        self._regularize_locations(face_location)
+        self._padding(15)
 
-for face_landmarks in face_landmarks_list:
+        tmp_pil_image = Image.fromarray(self.ary_image)
+        self.pil_image = tmp_pil_image.crop(self.box)
+        self.ary_image = numpy.array(self.pil_image)
 
-    # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+        print(str(self.box))
+        self.pil_image.show()
 
-    # Let's trace out each facial feature in the image with a line!
-    for facial_feature in face_landmarks.keys():
-        d.line(face_landmarks[facial_feature], width=5)
+    def _get_face_landmarks(self):
 
-# Show the picture
-pil_image.show()
+        self.face_landmarks_list = face_recognition.face_landmarks(self.ary_image)
+
+        print("I found {} face(s) in this photograph.".format(len(self.face_landmarks_list)))
+
+        for face_landmarks in self.face_landmarks_list:
+
+            # Print the location of each facial feature in this image
+            for facial_feature in face_landmarks.keys():
+                print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+    def _draw_facial_feature(self):
+
+        d_image = ImageDraw.Draw(self.pil_image)
+
+        for face_landmarks in self.face_landmarks_list:
+
+            for facial_feature in face_landmarks.keys():
+
+                d_image.line(face_landmarks[facial_feature], width = 3)
+
+        # Show the picture
+        self.pil_image.show()
+
+    def run(self):
+
+        self._crop_face()
+        self._get_face_landmarks()
+        self._draw_facial_feature()
+
+
+if __name__ == '__main__':
+
+    face = FaceId('pic/index.jpg')
+    face.run()
